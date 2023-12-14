3d37af366068d232f5e22966ebb1a55052f918df
mhkmhsh <mahekmahesh12@gmail.com>
2023-12-14 10:36:16 -0500

    CMMIT SAVE

---
+++ images/script.js
@@ -0,0 +1,11 @@
+function shakeMagic8Ball(){
+const question = prompt('Ask the 8 ball a question:')
+if ( question === null) {
+document.getElementById('response-text').innerText = 'You MUST ask a question'
+document.getElementById('response-text').classList = 'text-danger'
+document.getElementById('response-image').src = '#BADIMAGE'//ADD IMAGE LATER
+}
+
+
+
+}
