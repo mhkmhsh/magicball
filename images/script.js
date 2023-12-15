58208b0b3a30bba8131ed74efa875cb356be2cf2
mhkmhsh <mahekmahesh12@gmail.com>
2023-12-15 11:37:39 -0500

    change fix

--- images/script.js
+++ images/script.js
@@ -4,64 +4,64 @@ function shakeMagic8Ball() {
         document.getElementById('response-text').innerText = 'You MUST ask a question'
         document.getElementById('response-text').classList = 'text-danger'
         document.getElementById('response-image').src = '#BADIMAGE'//ADD IMAGE LATER
-     
+
     }
     if (!question.trim()) {
         alert('ENTER A QUESTION OR ELSE')
         return
     }
 
-    const randomNumber = Math.floor(Math.random()*8);
-let answer, image, color;
+    const randomNumber = Math.floor(Math.random() * 8);
+    let answer, image, color;
 
-switch(randomNumber){
-case 0:
-    answer = "It is certain";
-    image=" fix "
-    color="text-success"
-    break;
-  case 1:
-    answer = " Absolutely";
-    image=" fix "
-    color="text-success"
-    break;
-  case 2:
-    answer = "Reply hazy try again";
-    image=" fix "
-    color="text-info"
-    break;
-  case 3:
-    answer = "Cannot predict now";
-    image=" fix "
-    color="text-info"
-    break;
-  case 4:
-    answer = " D A N G E R";
-    image=" fix "
-    color="text-danger"
-    break;
-  case 5:
-    answer = "the spirits agree";
-    image=" fix "
-    color="text-primary"
-    break;
-  case 6:
-    answer = "signs convey no";
-    image=" fix "
-    color="text-danger"
-    break;
-  case 7:
-    answer = "Signs point to certain death";
-    image=" fix "
-    color="text-danger"
-    break;
-    default;
-    break;
+    switch (randomNumber) {
+        case 0:
+            answer = "It is certain";
+            image = " fix "
+            color = "text-success"
+            break;
+        case 1:
+            answer = " Absolutely";
+            image = " fix "
+            color = "text-success"
+            break;
+        case 2:
+            answer = "Reply hazy try again";
+            image = " fix "
+            color = "text-info"
+            break;
+        case 3:
+            answer = "Cannot predict now";
+            image = " fix "
+            color = "text-info"
+            break;
+        case 4:
+            answer = " D A N G E R";
+            image = " fix "
+            color = "text-danger"
+            break;
+        case 5:
+            answer = "the spirits agree";
+            image = " fix "
+            color = "text-primary"
+            break;
+        case 6:
+            answer = "signs convey no";
+            image = " fix "
+            color = "text-danger"
+            break;
+        case 7:
+            answer = "Signs point to certain death";
+            image = " fix "
+            color = "text-danger"
+            break;
+        default:
+
+    }
+    const responseText = document.getElementById('response-text');
+    responseText.innerText = answer;
+    responseText.classList = `display-4 ${color}`;
+    document.getElementById('response-image').src = image;
 }
-const responseText = document.getElementById('response-text');
- responseText.innerText = answer;
- responseText.classList = `display-4 ${color}`;
- document.getElementById('response-image').src = image;
-}
- 
+
 
